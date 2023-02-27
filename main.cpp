4ab4283a8fcdb8f09a78615db1544292812e968f
JNCJcoder <jorgenettocjunior@gmail.com>
2023-02-27 12:31:51 -0300

    Usando tabulações de tamanho 4

--- main.cpp
+++ main.cpp
@@ -3,18 +3,18 @@
 
 int main(int argc, char const *argv[])
 {
-    Chip8Disasm chip8;
+	Chip8Disasm chip8;
 
-    const bool fileExist = chip8.loadRom(argv[1]);
+	const bool fileExist = chip8.loadRom(argv[1]);
 
-    if(fileExist)
-    {
-        chip8.disasm();
-    }
-    else
-    {
-        std::cout << "Usage: " << argv[0] << " <ROM_LOCATION>" << std::endl;
-    }
-    
-    return 0;
+	if(fileExist)
+	{
+		chip8.disasm();
+	}
+	else
+	{
+		std::cout << "Usage: " << argv[0] << " <ROM_LOCATION>" << std::endl;
+	}
+	
+	return 0;
 }
